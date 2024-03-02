d04be274a89fb50ab7a5c4da2cb37f10bd34542a
Jordan <jordan.michalak17@gmail.com>
2024-03-02 08:20:15 +0100

    set up project structure with first component and add routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
