adb1cd23c1118f9da95bbbc2ff79fb1f180100ba
Szymon Blumczynski <szymon.blumczynski@gmail.com>
2024-03-07 21:08:29 +0100

    Update persistence request reply

--- backend/application-persistence/src/main/java/com/tower/aura/application/persistence/MetricsPersistenceRequest.java
+++ backend/application-persistence/src/main/java/com/tower/aura/application/persistence/MetricsPersistenceRequest.java
@@ -1,4 +1,7 @@
 package com.tower.aura.application.persistence;
 
-public record MetricsPersistenceRequest() {
+import com.tower.aura.application.core.MetricsCollection;
+import com.tower.aura.application.core.MetricsId;
+
+public record MetricsPersistenceRequest(MetricsId metricsId, MetricsCollection metricsCollection) {
 }
