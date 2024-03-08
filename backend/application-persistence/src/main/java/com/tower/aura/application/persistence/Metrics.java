2be04ab850ee050fe81b3be5d94c277748a0bef4
Szymon Blumczynski <szymon.blumczynski@gmail.com>
2024-03-08 21:02:00 +0100

    Move core classes to persistence

--- backend/application-persistence/src/main/java/com/tower/aura/application/persistence/Metrics.java
+++ backend/application-persistence/src/main/java/com/tower/aura/application/persistence/Metrics.java
@@ -1,4 +1,4 @@
-package com.tower.aura.application.core;
+package com.tower.aura.application.persistence;
 
 import java.util.Objects;
 
--- backend/application-persistence/src/main/java/com/tower/aura/application/persistence/MetricsPersistenceRequest.java
+++ backend/application-persistence/src/main/java/com/tower/aura/application/persistence/MetricsPersistenceRequest.java
@@ -1,7 +1,4 @@
 package com.tower.aura.application.persistence;
 
-import com.tower.aura.application.core.MetricsCollection;
-import com.tower.aura.application.core.MetricsId;
-
 public record MetricsPersistenceRequest(MetricsId metricsId, MetricsCollection metricsCollection) {
 }
