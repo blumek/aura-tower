f39287e8b74c14b8734756aff6e9ba523c03b9f4
Jordan <jordan.michalak17@gmail.com>
2024-03-11 22:05:46 +0100

    style no-results component

--- frontend/src/app/views/main-view/dashboard/dashboard.component.ts
+++ frontend/src/app/views/main-view/dashboard/dashboard.component.ts
@@ -7,108 +7,108 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
   mockData = [
-    // {
-    //   name: 'Janusz',
-    //   type: 'Odkurzacz',
-    //   location: 'Salon',
-    //   status: 'online',
-    //   data: {
-    //     power: '100',
-    //     temperature: '23',
-    //     humidity: '50',
-    //     co2: '100',
-    //     light: '100',
-    //   }
-    // },
-    // {
-    //   name: 'Xiaoomi Vacum Mi 2 PRO',
-    //   type: 'Odkurzacz',
-    //   location: 'Salon',
-    //   status: 'online',
-    //   data: {
-    //     power: '100',
-    //     water: '100',
-    //     weight: '100',
-    //     x: '100',
-    //     y: '100',
-    //     z: '100'
-    //   }
-    // },
-    // {
-    //   name: 'Xiaoomi Vacum Mi 2 PRO',
-    //   type: 'Odkurzacz',
-    //   location: 'Salon',
-    //   status: 'online',
-    //   data: {
-    //     power: '100',
-    //     water: '100',
-    //     weight: '100',
-    //     x: '100',
-    //     y: '100',
-    //     z: '100'
-    //   }
-    // },
-    // {
-    //   name: 'Xiaoomi Vacum Mi 2 PRO',
-    //   type: 'Odkurzacz',
-    //   location: 'Salon',
-    //   status: 'online',
-    //   data: {
-    //     power: '100',
-    //     water: '100',
-    //     weight: '100',
-    //     x: '100',
-    //     y: '100',
-    //     z: '100'
-    //   }
-    // },
-    // {
-    //   name: 'Xiaoomi Vacum Mi 2 PRO',
-    //   type: 'Odkurzacz',
-    //   location: 'Salon',
-    //   status: 'online',
-    //   data: {
-    //     power: '100',
-    //     water: '100',
-    //     weight: '100',
-    //     x: '100',
-    //     y: '100',
-    //     z: '100'
-    //   }
-    // },
-    // {
-    //   name: 'Seba',
-    //   type: 'Głośnik',
-    //   location: 'Salon',
-    //   status: 'offline',
-    //   data: {
-    //     power: '100',
-    //     temperature: '23',
-    //     humidity: '50',
-    //     co2: '100',
-    //     light: '100',
-    //     noise: '100',
-    //     pressure: '100',
-    //     sound: '100',
-    //     voltage: '100',
-    //     water: '100',
-    //     weight: '100',
-    //     x: '100',
-    //     y: '100',
-    //     z: '100'
-    //   }
-    // },    {
-    //   name: 'Philips Hue Z4S8E2S455',
-    //   type: 'Żarówka',
-    //   location: 'Salon',
-    //   status: 'online',
-    //   data: {
-    //     light: '100',
-    //     noise: '100',
-    //     sound: '100',
-    //     voltage: '100',
-    //     weight: '100',
-    //   }
-    // },
+    {
+      name: 'Janusz',
+      type: 'Odkurzacz',
+      location: 'Salon',
+      status: 'online',
+      data: {
+        power: '100',
+        temperature: '23',
+        humidity: '50',
+        co2: '100',
+        light: '100',
+      }
+    },
+    {
+      name: 'Xiaoomi Vacum Mi 2 PRO',
+      type: 'Odkurzacz',
+      location: 'Salon',
+      status: 'online',
+      data: {
+        power: '100',
+        water: '100',
+        weight: '100',
+        x: '100',
+        y: '100',
+        z: '100'
+      }
+    },
+    {
+      name: 'Xiaoomi Vacum Mi 2 PRO',
+      type: 'Odkurzacz',
+      location: 'Salon',
+      status: 'online',
+      data: {
+        power: '100',
+        water: '100',
+        weight: '100',
+        x: '100',
+        y: '100',
+        z: '100'
+      }
+    },
+    {
+      name: 'Xiaoomi Vacum Mi 2 PRO',
+      type: 'Odkurzacz',
+      location: 'Salon',
+      status: 'online',
+      data: {
+        power: '100',
+        water: '100',
+        weight: '100',
+        x: '100',
+        y: '100',
+        z: '100'
+      }
+    },
+    {
+      name: 'Xiaoomi Vacum Mi 2 PRO',
+      type: 'Odkurzacz',
+      location: 'Salon',
+      status: 'online',
+      data: {
+        power: '100',
+        water: '100',
+        weight: '100',
+        x: '100',
+        y: '100',
+        z: '100'
+      }
+    },
+    {
+      name: 'Seba',
+      type: 'Głośnik',
+      location: 'Salon',
+      status: 'offline',
+      data: {
+        power: '100',
+        temperature: '23',
+        humidity: '50',
+        co2: '100',
+        light: '100',
+        noise: '100',
+        pressure: '100',
+        sound: '100',
+        voltage: '100',
+        water: '100',
+        weight: '100',
+        x: '100',
+        y: '100',
+        z: '100'
+      }
+    },    {
+      name: 'Philips Hue Z4S8E2S455',
+      type: 'Żarówka',
+      location: 'Salon',
+      status: 'online',
+      data: {
+        light: '100',
+        noise: '100',
+        sound: '100',
+        voltage: '100',
+        weight: '100',
+      }
+    },
   ]
 }
