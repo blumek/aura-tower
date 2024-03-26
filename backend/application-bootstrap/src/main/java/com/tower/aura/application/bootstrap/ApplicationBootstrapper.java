e176517d0e054f2e69f4904ecfb18e709e0f83c6
Szymon Blumczynski <szymon.blumczynski@gmail.com>
2024-03-26 22:28:47 +0100

    Fix component scanning

--- backend/application-bootstrap/src/main/java/com/tower/aura/application/bootstrap/ApplicationBootstrapper.java
+++ backend/application-bootstrap/src/main/java/com/tower/aura/application/bootstrap/ApplicationBootstrapper.java
@@ -1,9 +1,11 @@
 package com.tower.aura.application.bootstrap;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import static org.springframework.boot.SpringApplication.run;
 
+@ComponentScan("com.tower.aura")
 @SpringBootApplication
 public class ApplicationBootstrapper {
     public static void main(String[] args) {
