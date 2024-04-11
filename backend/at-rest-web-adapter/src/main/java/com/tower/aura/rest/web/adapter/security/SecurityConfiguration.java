2fb534a92ffc4e9bbecad19661a18f6389b818f0
Szymon Blumczynski <szymon.blumczynski@gmail.com>
2024-04-11 13:06:55 +0200

    Add spring boot security

---
+++ backend/at-rest-web-adapter/src/main/java/com/tower/aura/rest/web/adapter/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.tower.aura.rest.web.adapter.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                );
+
+        return httpSecurity.build();
+    }
+}
