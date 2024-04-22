12705d99a7f36813d406979c27b81d617d295e9d
Jordan <jordan.michalak17@gmail.com>
2024-04-22 22:45:36 +0200

    init base container

--- frontend/src/app/views/base/base.module.ts
+++ frontend/src/app/views/base/base.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseComponent } from './base.component';
+import { AppRoutingModule } from '../../app-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { BaseComponent } from './base.component';
     BaseComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class BaseModule { }
