ab6bed8e927c7cd532d0a1d780f3f71ad54389db
Jordan <jordan.michalak17@gmail.com>
2024-04-27 23:53:42 +0200

    add steps with title description and frist animation for mobile version

--- frontend/src/app/shared/components/slider-stepper/slider-stepper.component.ts
+++ frontend/src/app/shared/components/slider-stepper/slider-stepper.component.ts
@@ -1,25 +1,29 @@
-import { NgClass } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-slider-stepper',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf],
   templateUrl: './slider-stepper.component.html',
   styleUrl: './slider-stepper.component.scss'
 })
 export class SliderStepperComponent implements OnInit {
   @Input() steps!: number;
   @Input() currentStep!: number;
+  @Output() currentStepChange = new EventEmitter<number>();
   stepsArray: number[] = [];
 
   ngOnInit(): void {
-    this.steps = this.steps < 0 ? 1 : this.steps;
-    this.currentStep = this.currentStep < 0 ? 1 : this.currentStep;
+    this.currentStep = this.currentStep <= 0 ? 0 : this.currentStep;
 
     for (let i = 1; i <= this.steps; i++) {
       this.stepsArray.push(i);
     }
+  }
+
+  changeStep(step: number) {
+    this.currentStepChange.emit(step);
   }
 
 
--- frontend/src/app/views/base/intro/intro.component.ts
+++ frontend/src/app/views/base/intro/intro.component.ts
@@ -3,18 +3,66 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.scss'
+  styleUrl: './intro.component.scss',
 })
-export class IntroComponent implements OnInit {
+export class IntroComponent {
   @ViewChild('imgElement') imgElement!: ElementRef;
 
-  imgSrc: string = '';
-  title: string = 'Witaj w Aura Tower';
-  description: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate suscipit ultrices. Vestibulum vehicula pellentesque justo sed ultrices. Suspendisse sed vestibulum tortor, sed aliquet arcu. Praesent ornare nibh sed metus varius laoreet. Donec interdum, ipsum ut ultrices lacinia, ex enim venenatis justo, quis dictum massa lorem quis diam. Morbi nec sodales nisi, ut malesuada mi. Morbi auctor nibh vitae orci porta vulputate. Sed accumsan nulla magna. Vestibulum semper justo a odio vestibulum, vel congue arcu lobortis.';
-  step: number = 1;
-
-  ngOnInit(): void {
-    
+  imgSrc: string = '../../../../assets/images/intro/welcome_to_at.svg';
+  title: string = '';
+  description: string = 'Przygotowaliśmy dla Ciebie krótkie wprowadzenie do Aura Tower. W kilku krokach pokażemy co umożliwia aplikacja i jak zacząć z niej korzystać. Zaczynamy?';
+  step: number = 0;
+  allSteps: number = 4;
+  visibleConfig = {
+    image: true,
+    title: true,
+    description: true,
+    button: true,
   }
 
+  changeStep(step?: number): void {
+    this.visibleConfig = {
+      image: false,
+      title: false,
+      description: false,
+      button: false,
+    }
+    this.step = step !== undefined ? step : this.step += 1;
+
+    switch (this.step) {
+      case 1:
+        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
+        this.title = 'Na co pozwoli ci Aura Tower?';
+        this.description = 'Moglibyśmy się rozpisywać ale w skrócie: Dodawaj, zarządzaj, steruj inteligentynymi urządzeniami w Twoim domu, biurze... Z poziomu dowolnego urządzenia przejdź do swojego dashboardu i zobacz soje urządzenia, dziel je na zdefiniowane przez siebie obszary i udostępniaj innym możliwość podglądu i sterowania urządzeniami z wybranych obszarów'
+        break;
+      case 2:
+        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
+        this.title = 'Stwóz własne centum dowodzenia';
+        this.description = 'Na początek stwórz własne centrum dowodzenia czyli miejsce w którym będziesz dodawać swoje inteligentne urządzenia. Chcesz podzielić urządzenia na te znajdujące się np. w mieszkaniu i te w biurze? Nie ma problemu - jedno centrum zarządzania jest odpowiednikiem jednego obszaru z Twoimi urządzeniami'
+        break;
+      case 3:
+        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
+        this.title = 'Wypełnij swój dashboard ';
+        this.description = 'W kolejnym etapie dodaj swoje urządzenia smart, przechodząd przez krótki proces konfiguratora, wybierając odpowiedni typ urządzenia, jego nazwę i wizualizację danych na ekranie'
+        break;
+      case 4:
+        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
+        this.title = 'Jesteś programistą?';
+        this.description = 'Aura Tower to tylko solidna podstawa w świecie IoT, rozwijaj swoje centum zarządzania dodając nowe wtyczki i obsługę nowych urządzeń  i funkcjonalności';
+        break;
+    }
+
+    setTimeout(() => {
+      this.visibleConfig = {
+        image: true,
+        title: true,
+        description: true,
+        button: true,
+      }
+    }, 2000)
+  }
+
+  skipIntro(): void {
+  
+  }
 }
