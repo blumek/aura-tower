e68c3cfab0cdeb1a43ba9dbfc9f609a5f507adb4
Jordan <jordan.michalak17@gmail.com>
2024-04-29 23:18:37 +0200

    intro - prepare desktop version

--- frontend/src/app/views/base/intro/intro.component.ts
+++ frontend/src/app/views/base/intro/intro.component.ts
@@ -1,68 +1,104 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StepData, VisibleConfig } from '../models/intro';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../../../shared/components/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
 })
-export class IntroComponent {
-  @ViewChild('imgElement') imgElement!: ElementRef;
-
+export class IntroComponent implements OnInit {
   imgSrc: string = '../../../../assets/images/intro/welcome_to_at.svg';
   title: string = '';
   description: string = 'Przygotowaliśmy dla Ciebie krótkie wprowadzenie do Aura Tower. W kilku krokach pokażemy co umożliwia aplikacja i jak zacząć z niej korzystać. Zaczynamy?';
   step: number = 0;
   allSteps: number = 4;
-  visibleConfig = {
-    image: true,
-    title: true,
-    description: true,
-    button: true,
+  visibleConfig: VisibleConfig = {
+    image: false,
+    title: false,
+    description: false,
+    button: false,
+  };
+  stepsData: StepData[] = [
+    {
+      imgSrc: '../../../../assets/images/intro/welcome_to_at.svg',
+      title: 'Na co pozwoli ci Aura Tower?',
+      description: 'Dodawaj, zarządzaj, steruj inteligentynymi urządzeniami w Twoim domu, biurze... Z poziomu dowolnego urządzenia przejdź do dashboardu i zobacz swoje urządzenia, dziel je na zdefiniowane obszary i udostępniaj innym użytkownikom z Twojego centrum zarządzania',
+    },
+    {
+      imgSrc: '../../../../assets/images/intro/welcome_to_at.svg',
+      title: 'Stwórz własne centrum dowodzenia',
+      description: 'Na początek stwórz własne centrum dowodzenia czyli miejsce w którym będziesz dodawać swoje inteligentne urządzenia. Chcesz podzielić urządzenia na te znajdujące się np. w mieszkaniu i te w biurze? Nie ma problemu - jedno centrum zarządzania jest odpowiednikiem jednego obszaru',
+    },
+    {
+      imgSrc: '../../../../assets/images/intro/welcome_to_at.svg',
+      title: 'Wypełnij swój dashboard',
+      description: 'W kolejnym etapie dodaj swoje urządzenia smart, przechodząc przez krótki proces konfiguratora, wybieraj odpowiedni typ urządzenia, jego nazwę i wizualizację danych na ekranie',
+    },
+    {
+      imgSrc: '../../../../assets/images/intro/welcome_to_at.svg',
+      title: 'Jesteś programistą?',
+      description: 'Aura Tower to tylko solidna podstawa w świecie IoT, rozwijaj swoje centum zarządzania dodając nowe wtyczki, obsługę nowych urządzeń i funkcjonalności',
+    },
+  ];
+
+  constructor(
+    private dialog: MatDialog,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.showElements();
   }
 
-  changeStep(step?: number): void {
+  hideAllElements(): void {
     this.visibleConfig = {
       image: false,
       title: false,
       description: false,
       button: false,
     }
-    this.step = step !== undefined ? step : this.step += 1;
+  }
 
-    switch (this.step) {
-      case 1:
-        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
-        this.title = 'Na co pozwoli ci Aura Tower?';
-        this.description = 'Moglibyśmy się rozpisywać ale w skrócie: Dodawaj, zarządzaj, steruj inteligentynymi urządzeniami w Twoim domu, biurze... Z poziomu dowolnego urządzenia przejdź do swojego dashboardu i zobacz soje urządzenia, dziel je na zdefiniowane przez siebie obszary i udostępniaj innym możliwość podglądu i sterowania urządzeniami z wybranych obszarów'
-        break;
-      case 2:
-        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
-        this.title = 'Stwóz własne centum dowodzenia';
-        this.description = 'Na początek stwórz własne centrum dowodzenia czyli miejsce w którym będziesz dodawać swoje inteligentne urządzenia. Chcesz podzielić urządzenia na te znajdujące się np. w mieszkaniu i te w biurze? Nie ma problemu - jedno centrum zarządzania jest odpowiednikiem jednego obszaru z Twoimi urządzeniami'
-        break;
-      case 3:
-        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
-        this.title = 'Wypełnij swój dashboard ';
-        this.description = 'W kolejnym etapie dodaj swoje urządzenia smart, przechodząd przez krótki proces konfiguratora, wybierając odpowiedni typ urządzenia, jego nazwę i wizualizację danych na ekranie'
-        break;
-      case 4:
-        this.imgSrc = '../../../../assets/images/intro/welcome_to_at.svg';
-        this.title = 'Jesteś programistą?';
-        this.description = 'Aura Tower to tylko solidna podstawa w świecie IoT, rozwijaj swoje centum zarządzania dodając nowe wtyczki i obsługę nowych urządzeń  i funkcjonalności';
-        break;
+  showElements(): void {
+    setTimeout(() => this.visibleConfig.image = true, 500);
+    setTimeout(() => this.visibleConfig.title = true, 2000);
+    setTimeout(() => this.visibleConfig.description = true, 3000);
+    setTimeout(() => this.visibleConfig.button = true, 5000);
+  }
+
+  changeStep(step?: number): void {
+    this.step = step !== undefined ? step : this.step + 1;
+
+    if (this.step <= this.allSteps) {
+        const stepData = this.stepsData[this.step - 1];
+        this.imgSrc = stepData.imgSrc;
+        this.title = stepData.title;
+        this.description = stepData.description;
     }
+  
+    this.hideAllElements();
+    this.showElements();
+  }
 
-    setTimeout(() => {
-      this.visibleConfig = {
-        image: true,
-        title: true,
-        description: true,
-        button: true,
-      }
-    }, 2000)
+  goToBaseCenter(): void {
+  
   }
 
   skipIntro(): void {
-  
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        title: 'Pominąć wprowadzenie?',
+        content: 'Jeśli pominiesz to wprowadzenie, nie będziesz mógł do niego wrócić w przyszłości',
+        cancelButtonText: 'Anuluj',
+        acceptButtonText: 'Pomiń'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
---
+++ frontend/src/app/views/base/models/intro.ts
@@ -0,0 +1,12 @@
+export interface StepData {
+    imgSrc: string,
+    title: string,
+    description: string
+}
+
+export interface VisibleConfig {
+    image: boolean,
+    title: boolean,
+    description: boolean,
+    button: boolean,
+}
