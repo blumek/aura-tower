a03041e69ee9cfb9289a9194be7b96ed2b02f11f
Jordan <jordan.michalak17@gmail.com>
2024-05-09 23:45:09 +0200

    set up interceptor config

--- frontend/src/app/views/auth/services/auth-interceptor.service.ts
+++ frontend/src/app/views/auth/services/auth-interceptor.service.ts
@@ -1,16 +1,68 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, concatMap, from, of, throwError } from 'rxjs';
 import { JwtTokenService } from './jwt-token.service';
+import { SnackbarService } from '../../../shared/services/snackbar.service';
+import { Router } from '@angular/router';
+import { TokenResponse } from '../models/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
+  constructor(
+    private jwtTokenService: JwtTokenService,
+    private snackBarService: SnackbarService,
+    private router: Router
+  ) {}
 
-  constructor(private jwtTokenService: JwtTokenService) { }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return from(this.handleRequest(req, next)).pipe(
+      catchError((error) => {
+        if (error.status === 500) {
+          this.snackBarService.openSnackBar('Wystąpił błąd', true);
+        }
+        return throwError(() => error);
+      })
+    );
+  }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req)
+  addAuthHeader(req: HttpRequest<any>, token: TokenResponse) {
+    return token
+      ? req.clone({ setHeaders: { Authorization: `Beearer ${token}` } })
+      : req;
+  }
+
+  handleRequest(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.jwtTokenService.isAccessTokenExpired()) {
+      if (this.jwtTokenService.isRefreshTokenExpired()) {
+        this.jwtTokenService.removeToken();
+        this.router.navigate(['/auth/sign-in']);
+        this.snackBarService.openSnackBar('Sesja wygasła', true);
+      } else {
+        this.jwtTokenService.refreshToken().pipe(
+          concatMap((token) => {
+            const newReq = this.addAuthHeader(req, token);
+
+            return next.handle(newReq);
+          })
+        );
+      }
+    } else {
+      const token = this.jwtTokenService.getToken();
+      const newReq = this.addAuthHeader(req, token);
+
+      return next.handle(newReq);
+    }
+
+    return next.handle(req);
   }
 }
--- frontend/src/app/views/auth/services/jwt-token.service.ts
+++ frontend/src/app/views/auth/services/jwt-token.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { TokenResponse } from '../models/auth';
+import { jwtDecode } from 'jwt-decode';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable, catchError, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JwtTokenService {
-  constructor() {}
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   setToken(token: TokenResponse) {
     if (token) {
@@ -13,10 +22,10 @@ export class JwtTokenService {
     }
   }
 
-  getToken(): string | null {
+  getToken(): any{
     const token = localStorage.getItem('token');
 
-    return token ? token : null;
+    return token ? JSON.parse(token) : null;
   }
 
   getAccessToken(): string | null {
@@ -25,7 +34,49 @@ export class JwtTokenService {
     return token ? JSON.parse(token).accessToken : null
   }
 
+  getRefreshToken(): string | null {
+    const token = localStorage.getItem('token');
+
+    return token ? JSON.parse(token).refreshToken : null
+  }
+
   removeToken(): void {
     localStorage.removeItem('token');
+  }
+
+  isAccessTokenExpired(): boolean {
+    const accessToken = this.getAccessToken();
+    const decodedTokenExp = this.decodeToken(accessToken).exp
+
+    return decodedTokenExp ? 1000 * Number(decodedTokenExp) - new Date().getTime() < 5000 : false;
+  }
+
+  isRefreshTokenExpired(): boolean {
+    const refreshToken = this.getRefreshToken();
+    const decodedTokenExp = this.decodeToken(refreshToken).exp
+
+    return decodedTokenExp ? 1000 * Number(decodedTokenExp) - new Date().getTime() < 5000 : false;
+  }
+
+  decodeToken(token: string | null): any {
+    return token ? jwtDecode(token) : null
+  }
+
+  refreshToken(): Observable<TokenResponse> {
+    const body = {
+      accessToken: this.getAccessToken(),
+      refreshToken: this.getRefreshToken()
+    }
+
+    return this.http.post<TokenResponse>(environment.authentication.refreshToken, body).pipe(
+      tap((token: TokenResponse) => {
+        this.setToken(token);
+      }),
+      catchError(err => {
+        this.removeToken();
+        this.router.navigate(['/auth/sign-in']);
+        throw err;
+      })
+    )
   }
 }
